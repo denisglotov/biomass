8406bbce09bb1daf51d9c95ab2a7d3a49483ab45
Denis Glotov <denis@glotov.org>
2026-08-13 16:17:14 +0300

    revamp game sound effects with ear-friendly sci-fi audio

--- src/audio.rs
+++ src/audio.rs
@@ -11,6 +11,8 @@ mod wasm_backend {
         fn play_sound_pop();
         fn play_sound_win();
         fn play_sound_loss();
+        fn play_sound_click();
+        fn play_sound_error();
     }
 
     pub struct SoundBackend;
@@ -28,6 +30,8 @@ mod wasm_backend {
                     SoundTrigger::IsolationPop => play_sound_pop(),
                     SoundTrigger::WinFanfare => play_sound_win(),
                     SoundTrigger::LossAlert => play_sound_loss(),
+                    SoundTrigger::ButtonClick => play_sound_click(),
+                    SoundTrigger::InvalidMove => play_sound_error(),
                 }
             }
         }
@@ -45,6 +49,8 @@ mod native_backend {
         snd_pop: Option<Sound>,
         snd_win: Option<Sound>,
         snd_loss: Option<Sound>,
+        snd_click: Option<Sound>,
+        snd_error: Option<Sound>,
     }
 
     impl SoundBackend {
@@ -65,6 +71,12 @@ mod native_backend {
                 snd_loss: load_sound_from_bytes(include_bytes!("../assets/loss.wav"))
                     .await
                     .ok(),
+                snd_click: load_sound_from_bytes(include_bytes!("../assets/click.wav"))
+                    .await
+                    .ok(),
+                snd_error: load_sound_from_bytes(include_bytes!("../assets/error.wav"))
+                    .await
+                    .ok(),
             }
         }
 
@@ -75,6 +87,8 @@ mod native_backend {
                 SoundTrigger::IsolationPop => &self.snd_pop,
                 SoundTrigger::WinFanfare => &self.snd_win,
                 SoundTrigger::LossAlert => &self.snd_loss,
+                SoundTrigger::ButtonClick => &self.snd_click,
+                SoundTrigger::InvalidMove => &self.snd_error,
             };
 
             if let Some(snd) = sound {
